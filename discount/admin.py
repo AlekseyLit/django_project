935eaf487d35d33a9ca52677907455c75d95d978
Alex Litiuk <alexlit1405@gmail.com>
2021-09-21 14:33:18 +0300

    Project start

--- discount/admin.py
+++ discount/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from discount import models as m
 # Register your models here.
+
+admin.site.register(m.Categories)
+admin.site.register(m.Discounts)
--- discount/models.py
+++ discount/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Discounts(models.Model):
+    product = models.CharField('Продукт', max_length=150)
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    brand = models.CharField('Брэнд', max_length=150)
+    model = models.CharField('Модель', max_length=150)
+    price = models.DecimalField('Цена', decimal_places=0, max_digits=20)
+    discount = models.FloatField('Скидка')
+    company = models.CharField('Компания', max_length=150)
+
+    def __str__(self):
+        return self.product + " " + self.model + " " + f"{self.price}" + " " + f"{self.discount}"
+
+    class Meta:
+        verbose_name = 'Скидка'
+        verbose_name_plural = 'Скидки'
