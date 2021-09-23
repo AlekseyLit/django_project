2d6d120d7f989bf68bb2c39b1c0830dd8fb68134
Alex Litiuk <alexlit1405@gmail.com>
2021-09-23 11:18:18 +0300

    Scheme Models of database

--- discount/models.py
+++ discount/models.py
@@ -1,8 +1,11 @@
+import datetime
+
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Categories(models.Model):
-    name = models.TextField()
+    name = models.TextField('Описание')
 
     def __str__(self):
         return self.name
@@ -27,3 +30,30 @@ class Discounts(models.Model):
     class Meta:
         verbose_name = 'Скидка'
         verbose_name_plural = 'Скидки'
+
+
+class Offers(models.Model):
+    company = models.CharField('Компания', max_length=150)
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    offer = models.TextField('Предложение')
+
+    def __str__(self):
+        return self.company + " " + self.offer
+
+    class Meta:
+        verbose_name = 'Предложение'
+        verbose_name_plural = 'Предложения'
+
+
+class Orders(models.Model):
+    user_id = models.ManyToManyField(User)
+    price = models.DecimalField('Сумма заказа', decimal_places=0, max_digits=20)
+    orderDate = models.DateTimeField('Время заказа', default=datetime.datetime.now())
+    deliveryDate = models.DateTimeField('Время доставки', default=datetime.datetime.now())
+
+    def __str__(self):
+        return f"{self.price}" + " " + f"{self.orderDate}" + f"{self.deliveryDate}"
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
